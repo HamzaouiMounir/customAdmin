d5a9df0fe7b7e3de43aa175177d3a52e469e8fc9
Mounir Hamzaoui <mounirhamzaoui@iMac-de-mounir.home>
2017-04-03 15:14:54 +0200

    Laravel 5.3 Forked

---
+++ routes/api.php
@@ -0,0 +1,33 @@
+<?php
+
+
+$api = app('Dingo\Api\Routing\Router');
+$api->version('v1', function ($api) {
+  $api->group(['middleware' => ['api']], function ($api) {
+      $api->resource('auth', 'App\Http\Controllers\Auth\AuthController');
+      $api->post('auth/login', 'App\Http\Controllers\Auth\AuthController@postLogin');
+
+      //facebook Oauth api call
+      //here we are sending to the server provider, id and the accessToken
+      $api->get('auth/{provider}/{oauthProviderId}/{accessToken}','App\Http\Controllers\Auth\AuthController@findUserByProviderToken');
+      // Password Reset Routes...
+      $api->post('auth/password/email', 'App\Http\Controllers\Auth\PasswordResetController@sendResetLinkEmail');
+      $api->get('auth/password/verify', 'App\Http\Controllers\Auth\PasswordResetController@verify');
+      $api->post('auth/password/reset', 'App\Http\Controllers\Auth\PasswordResetController@reset');
+      $api->get('notifications/refresh_token/{userId}/{device_notification_token}', 'App\Http\Controllers\Auth\AuthController@refreshDeviceNotificationToken');
+  });
+
+  /*$api->group(['middleware' => ['api', 'api.auth']], function ($api) {
+      $api->get('users/me', 'App\Http\Controllers\UserController@getMe');
+      $api->put('users/me', 'App\Http\Controllers\UserController@putMe');
+  });
+
+  $api->group(['middleware' => ['api', 'api.auth', 'role:admin.super|admin.user']], function ($api) {
+      $api->post('users', 'App\Http\Controllers\UserController@postUsers');
+      $api->resource('users', 'App\Http\Controllers\UserController');
+
+  });*/
+})
+
+
+ ?>
